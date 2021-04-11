e910c3da494e68b8f39f1a09ec19910a3271c4d4
BrahianCano <brahianestiven08@gmail.com>
2021-04-10 20:48:16 -0500

    change to puppeteer-core

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,28 +1,23 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-const puppeteer = require('puppeteer');
+const chromium = require("chrome-aws-lambda")
+const SEO_RESOLUTION = { width: 1200, height: 630 }
 
 
 export default async function handler(req, res) {
     if (req.method === 'POST') {
         const {body} = req;
 
-        const chromeOptions = {
+        const browser = await chromium.puppeteer.launch({
+            args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
+            defaultViewport: chromium.defaultViewport,
+            executablePath: await chromium.executablePath,
             headless: true,
-            defaultViewport: null,
-            args: [
-                "--incognito",
-                "--no-sandbox",
-                "--single-process",
-                "--no-zygote"
-            ],
-        };
+            ignoreHTTPSErrors: true,
+        })
 
-        const browser = await puppeteer.launch(chromeOptions);
-        const page = await browser.newPage();
-        await page.goto(body.url, {
-            timeout: 99999,
-                waitUntil: "networkidle2",
-        });
+        const page = await browser.newPage()
+        page.setViewport(SEO_RESOLUTION)
+        await page.goto(body.url,{ waitUntil: "networkidle0" });
 
         const searchValue = await page.$eval('h1', el => el.textContent);
 
