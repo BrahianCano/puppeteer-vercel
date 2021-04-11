28715a80b0df727c727b8c2adb34694507c2f394
BrahianCano <brahianestiven08@gmail.com>
2021-04-10 20:31:17 -0500

    first commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,34 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const puppeteer = require('puppeteer');
 
-export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
+
+export default async function handler(req, res) {
+    if (req.method === 'POST') {
+        const {body} = req;
+
+        const chromeOptions = {
+            headless: true,
+            defaultViewport: null,
+            args: [
+                "--incognito",
+                "--no-sandbox",
+                "--single-process",
+                "--no-zygote"
+            ],
+        };
+
+        const browser = await puppeteer.launch(chromeOptions);
+        const page = await browser.newPage();
+        await page.goto(body.url, {
+            timeout: 99999,
+                waitUntil: "networkidle2",
+        });
+
+        const searchValue = await page.$eval('h1', el => el.textContent);
+
+        await browser.close();
+
+        res.status(200).json({H1: searchValue})
+
+    }
 }
